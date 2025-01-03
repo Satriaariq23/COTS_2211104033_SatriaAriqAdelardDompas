90247082cac4aac4ec55c7642d36570e3cf8d7a2
Satriaariq23 <136548620+Satriaariq23@users.noreply.github.com>
2025-01-03 16:54:06 +0700

    Delete cots_2211104033 directory

--- cots_2211104033/android/app/src/main/kotlin/com/example/cots_2211104033/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cots_2211104033
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- cots_2211104033/lib/routes/app_routes.dart
+++
@@ -1,7 +0,0 @@
-class AppPages {
-  static final routes = [
-    GetPage(name: Routes.ONBOARDING, page: () => OnboardingPage()),
-    GetPage(name: Routes.LOGIN, page: () => LoginPage()),
-    GetPage(name: Routes.MAIN, page: () => MainPage()),
-  ];
-}
